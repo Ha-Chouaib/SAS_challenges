358bee97eb2e34dede056936034003befd6fab4b
Ha-hadadi <chouaib.hadadi01@gmail.com>
2026-09-01 15:30:12 +0200

    [Day 0 / Level 1] -> Age Logger

---
+++ Day0_Variables&&Conditions/Level_1/01_AgeLogger.js
@@ -0,0 +1,6 @@
+//Declare a variable age and assign a number to it. Log to the console: "You are [age] years old."
+
+let Age = 23;
+let result = `You are [ ${Age} ] years old.`;
+
+console.log(result);
