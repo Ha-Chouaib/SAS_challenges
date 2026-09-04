c72b29714edcbf1a12d3d2fec56bc45b7e98d053
Ha-hadadi <chouaib.hadadi01@gmail.com>
2026-09-04 16:19:13 +0200

    [update Day 0 Strings] -> update string compressor problem logic

--- Day03_Strings/Level_3/02_String Compressor.js
+++ Day03_Strings/Level_3/02_String Compressor.js
@@ -1,20 +1,33 @@
 // Write a function that compresses repeating characters (e.g., "aaabbc" becomes "a3b2c1").
 
-function removeStringDuplicates(str)
+function compressor(str)
 {
-    let s = "";
+    let count = 1;
+    let prev = str[0];
+    let result = "";
 
-    for(let i = 0; i < str.length; i ++)
-    {
-        if(!s.includes(str[i])) s+= str[i];
-    }
-    return s
+   for(let i = 0; i < str.length; i++)
+   {
+
+
+        if( str[i + 1] === prev )
+        {
+            count ++;
+        }else
+        {
+            result+= prev + count;
+            count =1;
+
+        }
+        prev = str[i + 1];
+   }
+   return result;
 }
 
 function displayResults()
 {
     let str = "aaabbbcc";
-    let compressed = removeStringDuplicates(str);
+    let compressed = compressor(str);
 
     console.log(str);
     console.log(compressed);
